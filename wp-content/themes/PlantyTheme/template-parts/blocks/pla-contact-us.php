ad96d748e0c152a2297bf265e9d889e72fae05ed
Annaelle <annaelleemery512@gmail.com>
2023-09-27 16:55:33 +0200

    First try emailing

--- wp-content/themes/PlantyTheme/template-parts/blocks/pla-contact-us.php
+++ wp-content/themes/PlantyTheme/template-parts/blocks/pla-contact-us.php
@@ -23,11 +23,25 @@ if (!empty($block['className'])) {
 
 
 $title = get_field('pla-contact-us_title') ?: 'Votre titre ici ...';
+$form_code = get_field('pla-contact-us_shortcode') ?: 'Votre shortcode ici...';
 
 ?>
 
 <!-- Structure du bloc “PLA - Nous contacter -->
-<section class="<?php echo esc_attr($className); ?> container" id="<?php echo esc_attr($id); ?>">
+<section class="<?php echo esc_attr($className); ?> bg-secondary pb-0 mb-0" id="<?php echo esc_attr($id); ?>">
+
+    <div class="container text-center d-flex flex-column justify-content-center align-items-center">
+
+        <h2 class="color-black"><?php echo $title ?></h2>
+
+        <div class="text-start">
+            <?php echo do_shortcode('' . $form_code . ''); ?>
+            <img class="mb-0 w-100 contact-us-flower" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/contact-us-flower.png'; ?>" width="147" height="134" alt="Plante sous formulaire de contact">
+        </div>
+
+    </div>
+
+    <img class="mb-0 mt-5 w-100" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/canettes.png'; ?>" width="1489" height="262" alt="Canettes Planty">
 
 
 </section>
